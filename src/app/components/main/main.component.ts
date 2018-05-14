8db43c04accba244e7ccd5cf8c8169257ca7c1b4
AlejandraCP <alejandracp710@gmail.com>
2018-05-14 18:06:50 -0500

    service

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HEROES } from '../../mock-heroes'
+import { HeroService } from '../../service/hero.service';
 
 @Component({
   selector: 'app-main',
@@ -16,15 +17,19 @@ export class MainComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[];
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
  
   ngOnInit() {
-    
+    this.getHeroes();
   }
   onSelect(costumer: Hero): void {
     this.selectedHero = costumer;
   }
-
+  
 }
