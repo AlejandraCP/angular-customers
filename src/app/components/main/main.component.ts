b0e12c3a10d8bea693622d64c793909bb712c959
AlejandraCP <alejandracp710@gmail.com>
2018-05-14 12:18:38 -0500

    style and selected

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HEROES } from '../../mock-heroes'
 
 @Component({
   selector: 'app-main',
@@ -8,14 +9,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  hero: Hero = {
-    id: 23,
-    name: "Andrea"
-  }
-  constructor() { }
-    
-  ngOnInit() {
-  }
+  // hero: Hero = {
+  //   id: 23,
+  //   name: "Andrea"
+  // }
 
+  selectedHero: Hero;
+
+  heroes = HEROES;
+  
+  constructor() { }
+ 
+  ngOnInit() {
+    
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
